0a31e97d5baa5391014c3aff1ee69a8cbef12db9
PriyabrataR <33996641+PriyabrataR@users.noreply.github.com>
2018-05-19 12:34:47 +0530

    dont need samosa

--- hungry.py
+++ hungry.py
@@ -1,9 +1,7 @@
 print ("Am I hungry")
-<<<<<<< HEAD
 print ("this is a file")
 print ("I am not hungry")
 print ("want burger")
-=======
 print ("not right now")
 Print ("need water")
->>>>>>> thirsty
+print ("dont need samosa")
